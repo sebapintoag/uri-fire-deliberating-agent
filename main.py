2a8bd492e006563aeec262c94b9574fa7a116821
Seba Pinto-Agüero González <sebastian.pintoaguero@usach.cl>
2023-12-10 12:43:20 -0300

    format code

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import deliberating_agent as da
 
 # Inputs
 drone_data = {
-    'risk_factor': 'trash',
-    'surroundings': 'dry_grass',
-    'location': [-33.051876, -71.547227],
+    "risk_factor": "trash",
+    "surroundings": "dry_grass",
+    "location": [-33.051876, -71.547227],
 }
 
 weather_data = {
-    'temperature': 29.0,
-    'wind_speed': 30.0,
-    'humidity': 0.8,
-    'place': 'Valparaiso'
+    "temperature": 29.0,
+    "wind_speed": 30.0,
+    "humidity": 0.8,
+    "place": "Valparaiso",
 }
 
-print('-> Input data')
-print('Drone data: ' + str(drone_data))
-print('Weather data: ' + str(weather_data))
+print("-> Input data")
+print("Drone data: " + str(drone_data))
+print("Weather data: " + str(weather_data))
 
 # Evaluate situation and make decisions
 situation = da.evaluate_situation(drone_data, weather_data)
 decisions = da.make_decisions(situation)
 
 # Print output
-print('\n-> Results')
+print("\n-> Results")
 print(decisions)
